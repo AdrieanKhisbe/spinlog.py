73e6fa8053cc26e120c025454108f9b2960ced98
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-24 15:19:29 +0200

    Handling of multi line text, preserving indentation

--- spinlog/core.py
+++ spinlog/core.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from halo import Halo
-
+import textwrap
 
 class LogProgress():
     def __init__(self, message):
@@ -12,23 +12,27 @@ class LogProgress():
     def __exit__(self, type, value, traceback):
         self.spinner.stop()
 
-
+def handle_multiline(string):
+    if "\n" in string:
+        return textwrap.indent(string, '  ')[2:]
+    else:
+        return string
 
 class SpinLogger():
     def __init__(self, spinner):
         self._spinner = spinner
 
     def error(self, message):
-        self._spinner.fail(message).start()
+        self._spinner.fail(handle_multiline(message)).start()
 
     def warn(self, message):
-        self._spinner.warn(message).start()
+        self._spinner.warn(handle_multiline(message)).start()
 
     def info(self, message):
-        self._spinner.info(message).start()
+        self._spinner.info(handle_multiline(message)).start()
 
     def debug(self, message):
-        self._spinner.stop_and_persist(symbol="🐛".encode("utf-8"), text=message).start()
+        self._spinner.stop_and_persist(symbol="🐛".encode("utf-8"), text=handle_multiline(message)).start()
 
     def log(self, message, symbol=" "):
-        self._spinner.stop_and_persist(symbol=symbol, text=message).start()
+        self._spinner.stop_and_persist(symbol=symbol, text=handle_multiline(message)).start()
