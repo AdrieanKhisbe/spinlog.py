dd43a855621e4430150e3e9ea8d38abbcf45ee69
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-28 09:40:41 +0200

    Start to write first spinner test, but will need to configure halo stream

--- tests/spinlog_test.py
+++ tests/spinlog_test.py
@@ -1,14 +1,7 @@
-from spinlog.core import handle_multiline
+from spinlog.core import Spinner, LogProgress, SpinLogger
 
 
-def test_handle_multiline_with_single_line():
-    tests = ["ABC", "Text with space", "Another one bites the dust"]
-    for sample_text in tests:
-        assert handle_multiline(sample_text) == sample_text
-
-
-def test_handle_multiline_with_multiline_line():
-    assert (handle_multiline("Text with newline\nbecause I have lot to tell") ==
-            "Text with newline\n  because I have lot to tell")
-    assert (handle_multiline("A\nB\nC") ==
-            "A\n  B\n  C")
+# def test_spinner_get_return_SpinLogger_with_message():
+#     spinner = Spinner.get("message")
+#     isinstance(spinner, SpinLogger)
+# Cause ValueError: I/O operation on closed file
